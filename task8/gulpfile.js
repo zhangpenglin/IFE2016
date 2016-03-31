bffe8b8fd54a37e51d3d8980013e78427fd52a2b
Zhang Penglin <88379382@qq.com>
2016-03-31 16:26:11 +0800

    task10

---
+++ task8/gulpfile.js
@@ -0,0 +1,36 @@
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var sass = require('gulp-sass');
+var minifyCss = require('gulp-minify-css');
+var rename = require('gulp-rename');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var processors = [autoprefixer];
+var plumber = require('gulp-plumber');
+var paths = {
+  sass: ['./scss/*.scss']
+};
+
+
+gulp.task('default', ['sass']);
+
+gulp.task('sass', function(done) {
+  gulp.src('./scss/style.scss')
+  .pipe(plumber())
+    .pipe(sass())
+    .on('error', sass.logError)
+    .pipe(gulp.dest('./css/'))
+    .pipe(minifyCss({
+      keepSpecialComments: 0
+    }))
+    .pipe(rename({ extname: '.min.css' }))
+    .pipe(gulp.dest('./css/'))
+    .on('end', done);
+});
+
+
+gulp.task('watch', function() {
+  gulp.watch(paths.sass, ['sass']);
+});
+
+
